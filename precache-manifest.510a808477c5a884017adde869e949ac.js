c6482bb48d5004430d10c95749e0bf4a0911d724
kkatsi <kotsoulis@gmail.com>
2020-08-28 20:27:16 +0300

    Updates

---
+++ precache-manifest.510a808477c5a884017adde869e949ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfde4ad1ef8dd87e7265daec5fa7d8fc",
+    "url": "/weirdImagesGallery/index.html"
+  },
+  {
+    "revision": "ab7a8ffed70df519f960",
+    "url": "/weirdImagesGallery/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "87f9777a79053cfd5792",
+    "url": "/weirdImagesGallery/static/css/main.74ed352f.chunk.css"
+  },
+  {
+    "revision": "ab7a8ffed70df519f960",
+    "url": "/weirdImagesGallery/static/js/2.123c5955.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weirdImagesGallery/static/js/2.123c5955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f9777a79053cfd5792",
+    "url": "/weirdImagesGallery/static/js/main.4f55ed4a.chunk.js"
+  },
+  {
+    "revision": "cf400768efd04811ef5e",
+    "url": "/weirdImagesGallery/static/js/runtime-main.b5df012f.js"
+  }
+]);
